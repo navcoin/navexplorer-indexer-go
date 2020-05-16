a0926cd5b19cac1b22fb88a229dd7ce76af1b77d
dantudor <dtudor01@gmail.com>
2020-05-16 20:32:54 +0100

    Adds GetBlockByHash

--- internal/service/block/repository.go
+++ internal/service/block/repository.go
@@ -39,6 +39,32 @@ func (r *Repository) GetHeight() (uint64, error) {
 	return block.Height, nil
 }
 
+func (r *Repository) GetBlockByHash(hash string) (*explorer.Block, error) {
+	results, err := r.Client.
+		Search(elastic_cache.BlockIndex.Get()).
+		Query(elastic.NewMatchQuery("hash", hash)).
+		Size(1).
+		Do(context.Background())
+	if err != nil || results == nil {
+		raven.CaptureError(err, nil)
+		return nil, err
+	}
+
+	if len(results.Hits.Hits) == 0 {
+		raven.CaptureError(err, nil)
+		return nil, elastic_cache.ErrRecordNotFound
+	}
+
+	var block *explorer.Block
+	hit := results.Hits.Hits[0]
+	if err = json.Unmarshal(hit.Source, &block); err != nil {
+		raven.CaptureError(err, nil)
+		return nil, err
+	}
+
+	return block, nil
+}
+
 func (r *Repository) GetBlockByHeight(height uint64) (*explorer.Block, error) {
 	results, err := r.Client.
 		Search(elastic_cache.BlockIndex.Get()).
