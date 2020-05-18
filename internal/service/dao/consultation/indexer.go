527f9ebd4b090ad1dc2fa1d971e65d23c1ddbec4
dantudor <dtudor01@gmail.com>
2020-05-19 00:17:19 +0100

    Update Consultation indexer

--- internal/service/dao/consultation/indexer.go
+++ internal/service/dao/consultation/indexer.go
@@ -6,18 +6,22 @@ import (
 	"github.com/NavExplorer/navcoind-go"
 	"github.com/NavExplorer/navexplorer-indexer-go/internal/config"
 	"github.com/NavExplorer/navexplorer-indexer-go/internal/elastic_cache"
+	"github.com/NavExplorer/navexplorer-indexer-go/internal/service/block"
+	"github.com/NavExplorer/navexplorer-indexer-go/internal/service/dao/consensus"
 	"github.com/NavExplorer/navexplorer-indexer-go/pkg/explorer"
 	"github.com/getsentry/raven-go"
 	log "github.com/sirupsen/logrus"
+	"strconv"
 )
 
 type Indexer struct {
-	navcoin *navcoind.Navcoind
-	elastic *elastic_cache.Index
+	navcoin         *navcoind.Navcoind
+	elastic         *elastic_cache.Index
+	blockRepository *block.Repository
 }
 
-func NewIndexer(navcoin *navcoind.Navcoind, elastic *elastic_cache.Index) *Indexer {
-	return &Indexer{navcoin, elastic}
+func NewIndexer(navcoin *navcoind.Navcoind, elastic *elastic_cache.Index, blockRepository *block.Repository) *Indexer {
+	return &Indexer{navcoin, elastic, blockRepository}
 }
 
 func (i *Indexer) Index(txs []*explorer.BlockTransaction) {
@@ -40,7 +44,9 @@ func (i *Indexer) Index(txs []*explorer.BlockTransaction) {
 				log.WithError(err).Fatal("Failed to save new consultation")
 			}
 
-			Consultations = append(Consultations, consultation)
+			Consultations.Add(consultation)
+		} else {
+			log.WithField("hash", tx.Hash).WithError(err).Error("Failed to find consultation")
 		}
 	}
 }
@@ -59,14 +65,44 @@ func (i *Indexer) Update(blockCycle *explorer.BlockCycle, block *explorer.Block)
 
 		if UpdateConsultation(navC, c) {
 			c.UpdatedOnBlock = block.Height
-			log.Debugf("Consultation %s updated on block %d", c.Hash, block.Height)
+			log.Infof("Consultation %s updated on block %d", c.Hash, block.Height)
 			i.elastic.AddUpdateRequest(elastic_cache.DaoConsultationIndex.Get(), c)
 		}
 
-		if c.Status == "expired" {
+		if c.ConsensusParameter && uint(c.State) == explorer.ConsultationPassed.State && c.HasPassedAnswer() && c.StateChangedOnBlock == block.Hash {
+			c.UpdatedOnBlock = block.Height
+			i.updateConsensusParameter(c, block)
+			i.elastic.AddUpdateRequest(elastic_cache.DaoConsultationIndex.Get(), c)
+			Consultations.Delete(c.Hash)
+		}
+
+		if uint(c.State) == explorer.ConsultationExpired.State {
 			if block.Height-c.UpdatedOnBlock >= uint64(blockCycle.Size) {
 				Consultations.Delete(c.Hash)
 			}
 		}
 	}
+}
+
+func (i *Indexer) updateConsensusParameter(c *explorer.Consultation, b *explorer.Block) {
+	answer := c.GetPassedAnswer()
+	if answer == nil {
+		log.WithField("consultation", c).Fatal("Passed Consultation with no passed answer")
+		return
+	}
+
+	parameter := consensus.Parameters.GetById(c.Min)
+	if parameter == nil {
+		log.WithField("consultation", c).Fatal("Consensus parameter not found")
+		return
+	}
+
+	value, _ := strconv.Atoi(answer.Answer)
+	parameter.Value = value
+	parameter.UpdatedOnBlock = b.Height
+	log.WithFields(log.Fields{
+		"parameter": parameter.Description,
+		"value":     parameter.Value,
+		"height":    b.Height,
+	}).Info("Updated Consensus Parameter")
 }
