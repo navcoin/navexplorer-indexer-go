6fca40029534109d5ecd9d368205a95505d8deac
dantudor <dtudor01@gmail.com>
2020-05-19 00:15:00 +0100

    Update delete consultation

--- internal/service/dao/consultation/consultations.go
+++ internal/service/dao/consultation/consultations.go
@@ -1,18 +1,23 @@
 package consultation
 
-import "github.com/NavExplorer/navexplorer-indexer-go/pkg/explorer"
+import (
+	"github.com/NavExplorer/navexplorer-indexer-go/pkg/explorer"
+)
 
 var Consultations consultations
 
 type consultations []*explorer.Consultation
 
+func (p *consultations) Add(c *explorer.Consultation) {
+	Consultations = append(Consultations, c)
+}
+
 func (p *consultations) Delete(hash string) {
 	for i := range Consultations {
 		if Consultations[i].Hash == hash {
 			Consultations[i] = Consultations[len(Consultations)-1]
-			Consultations[len(Consultations)-1] = nil
-			Consultations = append([]*explorer.Consultation(nil), Consultations[:len(Consultations)-1]...)
-			break
+			Consultations = Consultations[:len(Consultations)-1]
+			return
 		}
 	}
 }
