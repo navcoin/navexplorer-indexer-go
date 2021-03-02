14ba88b5792d1c646722788689dd5b46d84d9641
Dan Tudor <dtudor01@gmail.com>
2021-03-02 20:37:24 +0000

    unique addresses

--- internal/service/address/indexer.go
+++ internal/service/address/indexer.go
@@ -107,7 +107,19 @@ func getAddressesForTxs(txs []*explorer.BlockTransaction) []string {
 		}
 	}
 
-	return addresses
+	return unique(addresses)
+}
+
+func unique(stringSlice []string) []string {
+	keys := make(map[string]bool)
+	var list []string
+	for _, entry := range stringSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
 }
 
 func getTxById(id string, txs []*explorer.BlockTransaction) *explorer.BlockTransaction {
